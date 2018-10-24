431a65b93ae3783cfb0e678d261388a8dc4a9c3b
Amine195 <gmadhgs@gmail.com>
2018-10-24 13:20:22 +0100

    ignore

--- routes/index.js
+++ routes/index.js
@@ -3,38 +3,57 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', (req, res, next) => {
-  res.render('index', { title: 'Vizishop - Acceuil' });
+  res.send('Get Home');
 });
 
 /* GET Regsiter. */
 router.get('/register', (req, res, next) => {
-  res.render('index', { title: 'Vizishop - Acceuil' });
+  res.send('Get Register');
 });
-
 
 /* POST Register. */
 router.post('/register', (req, res, next) => {
-  res.render('index', { title: 'Vizishop - Acceuil' });
+  res.send('Post Register');
 });
 
 /* GET Login. */
 router.get('/login', (req, res, next) => {
-  res.render('index', { title: 'Vizishop - Acceuil' });
+  res.send('Get login');
 });
 
 /* POST Login. */
 router.post('/login', (req, res, next) => {
-  res.render('index', { title: 'Vizishop - Acceuil' });
+  res.send('Post Login');
 });
 
 /* GET Profile. */
 router.get('/profile', (req, res, next) => {
-  res.render('index', { title: 'Vizishop - Acceuil' });
+  res.send('Get profile');
 });
 
 /* PUT Profile. */
 router.put('/profile/:user_id', (req, res, next) => {
-  res.render('index', { title: 'Vizishop - Acceuil' });
+  res.send('Put profile');
+});
+
+/* GET Forgot Password. */
+router.get('/forgot-pw', (req, res, next) => {
+  res.send('Get forgot');
+});
+
+/* PUT Forgot Password. */
+router.put('/forgot-pw', (req, res, next) => {
+  res.send('Put forgot');
+});
+
+/* GET Forgot Password. */
+router.get('/reset-pw/:token', (req, res, next) => {
+  res.send('Get forgot');
+});
+
+/* PUT Forgot Password. */
+router.put('/reset-pw/:token', (req, res, next) => {
+  res.send('Put forgot');
 });
 
 module.exports = router;
