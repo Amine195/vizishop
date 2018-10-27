623bd2872206e2b689f5442e9f2cee42771ea479
Amine195 <gmadhgs@gmail.com>
2018-10-27 21:26:06 +0100

    Register 2

--- controllers/index.js
+++ controllers/index.js
@@ -1,14 +1,49 @@
 const User = require('../models/user');
 
 module.exports = {
-    postRegister(req, res, nex) {
-        User.register(new User({username: req.body.username}), req.body.password, (err) => {
-            if (err) {
-                console.log('error while user register!', err);
-                return next(err);
-            }
-            console.log('registred');
-            res.redirect('/');
+    async postRegister(req, res, nex) {
+
+        const newUser = new User({
+            username: req.body.username,
+            email: req.body.email,
+            image: req.body.image
         });
+
+        await User.register(newUser, req.body.password);
+        res.redirect('/');
     }
 }
+
+/******************************************************** METHODE 1 */
+// postRegister(req, res, nex) {
+
+//     const newUser = new User({
+//         username: req.body.username,
+//         email: req.body.email,
+//         image: req.body.image
+//     });
+
+//     User.register(newUser, req.body.password, (err) => {
+
+//         if (err) { return next(err); }
+
+//         res.redirect('/');
+//     });
+// }
+
+/******************************************************** METHODE 2 */
+// async postRegister(req, res, nex) {
+
+//     const newUser = new User({
+//         username: req.body.username,
+//         email: req.body.email,
+//         image: req.body.image
+//     });
+
+//     try {
+//         await User.register(newUser, req.body.password);
+//     } catch(err) {
+//         return next(err);
+//     }
+//     res.redirect('/');
+// }
